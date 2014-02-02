b6fe7381c66aa7c299024c3c3ded3c464e6e2730
Tad Worthington <harrylippy@gmail.com>
2014-02-02 00:45:23 -0600

    Prettied up Tiger.

--- src/Tiger/Types.go
+++ src/Tiger/Types.go
@@ -1,41 +1,42 @@
 package Tiger
 
 import (
-	"encoding/binary"
+    "encoding/binary"
 )
 
 // The 192-bit hash value
 type value struct {
-	a, b, c    uint64
-	aa, bb, cc uint64
+    a, b, c    uint64
+    aa, bb, cc uint64
 }
 
 func (v *value) Bytes() []byte {
-	var b []byte
+    var b []byte
 
-	buf := make([]byte, 8)
+    buf := make([]byte, 8)
 
-	binary.LittleEndian.PutUint64(buf, v.a)
-	b = append(b, buf...)
-	binary.LittleEndian.PutUint64(buf, v.b)
-	b = append(b, buf...)
-	binary.LittleEndian.PutUint64(buf, v.c)
-	b = append(b, buf...)
+    binary.LittleEndian.PutUint64(buf, v.a)
+    b = append(b, buf...)
+    binary.LittleEndian.PutUint64(buf, v.b)
+    b = append(b, buf...)
+    binary.LittleEndian.PutUint64(buf, v.c)
+    b = append(b, buf...)
 
-	return b
+    return b
 }
 
 // Tiger blocks are 512-bits (8 64-bit words)
 type block struct {
-	x [8]uint64
+    x [8]uint64
 }
 
 type Hash struct {
-	M      []block
-	N      int
-	digest value
+    M      []block
+    N      int
+    digest value
 }
 
 func (h *Hash) Digest() []byte {
-	return h.digest.Bytes()
+    return h.digest.Bytes()
 }
+
